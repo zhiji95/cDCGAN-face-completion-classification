17ee97192cb6ac7bb668f3caf5ad0d4c16bdc761
Zhi Ji <33732707+zhiji95@users.noreply.github.com>
2019-01-23 21:00:09 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,27 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os, sys
+import pandas as pd
+sys.path.append('utils')
+from nets import *
+# from datas import *
+import os,sys
+from PIL import Image
+import scipy.misc
+from glob import glob
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pandas as pd
+
+
 sample_dir = 'Samples/celebA_cgan_classifier_advance300/dia1:1'
 ckpt_dir = 'Models/celebA_cgan_classifier_advance300/dia1:1'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
